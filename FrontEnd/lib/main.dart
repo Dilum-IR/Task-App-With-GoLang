df61cbbd3d4b99f7411fda427289fd4d59c7a33e
LearnCsWithDIR <rdinduwara19158@gmail.com>
2023-11-25 22:18:54 +0530

    Project Initialization

---
+++ FrontEnd/lib/main.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/material.dart';
+import 'package:task_managment/screens/home.dart';
+
+void main() {
+  runApp(const TaskManagementApp());
+}
+
+class TaskManagementApp extends StatelessWidget {
+  const TaskManagementApp({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: const Home(),
+    );
+  }
+}
